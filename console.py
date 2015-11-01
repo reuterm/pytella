64af9eb1580ed413a179f684c89eb6ea558ef3e9
Max Reuter <max.reuter@uni-ulm.de>
2015-11-01 15:05:48 +0200

    clean up

--- console.py
+++ console.py
@@ -1,58 +1,102 @@
 import sys
-from p2p import MsgTypes
-from p2p import P2PConnection
 import socket
+import select
+import binascii
+from p2p import parseReceivedMessage, createMessage
+
+MSG_PING = 0x00
+MSG_PONG = 0x01
+MSG_BYE = 0x02
+MSG_JOIN = 0x03
+MSG_QUERY = 0x80
+MSG_QHIT = 0x81
 
 
 # =============================================================================
 # Simple console program that makes it easy to use our p2p network.
 # =============================================================================
+class p2pConsole:
+    def __init__(self):
+        # initialize p2p connection with default values
+        self.port = 6346
+        self.host = socket.gethostbyname(socket.gethostname())
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sequence_nr = 0
+        self.org_port = 0
+        self.org_ip = '0.0.0.0'
+
+    def start_connection(self):
+        try:
+            self.sock.connect((str(self.host), self.port))
+            self.org_ip = self.sock.getsockname()[0]
+            self.org_port = self.sock.getsockname()[1]
+            print ("Connected to " + str(self.host) + " with port : " +
+                   str(self.org_port))
+        except:
+            print("Connection failed or it is already connected!")
+
+    def close_connection(self):
+        self.sock.close()
+        print("Connection closed")
+
+    def receive_message(self):
+        ready = select.select([self.sock], [], [], 10.0)
+        if ready[0]:
+            print('Starting to receive')
+            reply = self.sock.recv(4096)  # 4096
+            print('Reply raw:')
+            print(binascii.hexlify(reply))
+            print('Reply parsed:')
+            print(parseReceivedMessage(reply))
+        else:
+            print('Timeout!')
+
+
 def main():
-    # initialize p2p connection with default values
-    port = 6346
-    msg_types = MsgTypes()
-    # my ip connect 192.168.56.1 6346
-    p2p = P2PConnection(port, socket.gethostbyname(socket.gethostname()))
-#    p2p.start_connection()
+    app = p2pConsole()
     while(True):
         console_input = str(raw_input('node$ ')).split()
-        print(console_input)
-        if str(console_input[0]) == 'c':
+        if str(console_input[0]) == 'connect':
             if len(console_input) == 3:
-                p2p.host = str(console_input[1])
-                p2p.org_port = int(console_input[2])
-                p2p.start_connection()
+                app.host = str(console_input[1])
+                app.port = int(console_input[2])
+                app.start_connection()
             else:
-                print("Staring connection with default values. Use following" +
-                      " syntax for custom values: connect [host] [port] ")
-                p2p.start_connection()
+                print("Staring connection with default values. Use " +
+                      "following syntax for custom values: connect " +
+                      "[host] [port]")
+                app.start_connection()
         elif str(console_input[0]) == 'join':
-            p2p.send_message(p2p.createMessage(
-                msg_type=msg_types.get_MSG_JOIN(), payload=""))
-            p2p.receive_message()
-        elif str(console_input[0]) == 'query':  # example: vm3testkey
+            app.sock.send(createMessage(
+                msg_type=MSG_JOIN, payload="", org_port=app.org_port,
+                org_ip=app.org_ip))
+            app.receive_message()
+        elif str(console_input[0]) == 'query':
             if len(console_input) == 2:
-                message = p2p.createMessage(
-                    msg_type=msg_types.get_MSG_QUERY(),
-                    payload=str(console_input[1]), ttl=3)
-                print(p2p.parseReceivedMessage(message))
-                p2p.send_message(message)
-                p2p.receive_message()
+                message = createMessage(
+                    msg_type=MSG_QUERY, ttl=3, org_port=app.org_port,
+                    org_ip=app.org_ip, payload=str(console_input[1]))
+                print(parseReceivedMessage(message))
+                app.sock.send(message)
+                app.receive_message()
             else:
                 print("Use following syntax: query [payload]")
         elif str(console_input[0]) == 'ping':
-            p2p.send_message(p2p.createMessage(ttl=3))
-            p2p.receive_message()
+            app.sock.send(createMessage(ttl=3, org_port=app.org_port,
+                                        org_ip=app.org_ip))
+            app.receive_message()
         elif str(console_input[0]) == 'bye':
             print("Closing connection")
-            p2p.send_message(p2p.createMessage(
-                msg_type=msg_types.get_MSG_BYE()))
-            p2p.close_connection()
+            app.sock.send(createMessage(msg_type=MSG_BYE,
+                                        org_port=app.org_port,
+                                        org_ip=app.org_ip))
+            app.close_connection()
             print("Closed")
-        elif str(console_input[0]) == 'q':
-            p2p.close_connection()
+        elif str(console_input[0]) == 'quit':
+            app.close_connection()
             sys.exit(0)
         else:
             print('Wrong input!')
+
 
 main()
--- node.py
+++ node.py
@@ -2,11 +2,12 @@ import socket
 import threading
 import struct
 import binascii
-from hashlib import md5
-import time
+import sys
+from p2p import parseReceivedMessage, createMessage, ipToNum
 
 # =============================================================================
 # Script for creating a node for the P2P network. Connects to 130.233.195.30
+# by default.
 # =============================================================================
 
 # List of known neighbours
@@ -41,7 +42,8 @@ def process_message(header, payload, socket):
             if header[1] == 1:
                 print("Respond with PONG A")
                 socket.send(createMessage(msg_type=MSG_PONG,
-                                          org_port=header[4]))
+                                          org_port=socket.getsockname()[1],
+                                          org_ip=socket.getsockname()[0]))
             # Type b
             else:
                 tmp = list(neighbours)
@@ -56,7 +58,9 @@ def process_message(header, payload, socket):
                                                 item[1], 0))
                 print("Respond with PONG B")
                 socket.send(createMessage(msg_type=MSG_PONG,
-                                          org_port=header[4], payload=data))
+                                          org_port=socket.getsockname()[1],
+                                          org_ip=socket.getsockname()[0],
+                                          payload=data))
         elif header[2] == MSG_PONG:
             # Pong type b
             if len(payload) > 0:
@@ -82,7 +86,9 @@ def process_message(header, payload, socket):
             data.append(0x02)
             data.append(0x00)
             print("Respond with JOIN OK")
-            socket.send(createMessage(msg_type=MSG_JOIN, org_port=header[4],
+            socket.send(createMessage(msg_type=MSG_JOIN,
+                                      org_port=socket.getsockname()[1],
+                                      org_ip=socket.getsockname()[0],
                                       payload=data))
         elif header[2] == MSG_QUERY:
             # Check if key is known
@@ -91,8 +97,9 @@ def process_message(header, payload, socket):
                                    binascii.unhexlify(keys[payload[0]]))
                 print("Respond with QUERY HIT")
                 socket.send(createMessage(msg_type=MSG_QHIT,
-                                          org_port=header[4], payload=data,
-                                          msg_id=header[7]))
+                                          org_port=socket.getsockname()[1],
+                                          org_ip=socket.getsockname()[0],
+                                          payload=data, msg_id=header[7]))
             # Save search
             q_searches.append((header[6], header[7]))
             # Forward to other nodes
@@ -119,7 +126,6 @@ def process_message(header, payload, socket):
 
 # Forward messages to existing connections
 def forward(header, payload, sock):
-
     global connections
     data = struct.pack('>'+str(len(payload[0]))+'s', payload[0])
     message = createMessage(msg_type=MSG_QUERY, ttl=header[1]-1,
@@ -134,70 +140,9 @@ def forward(header, payload, sock):
             connection[1].send(message)
 
 
-# Create binary data to be sent
-def createMessage(msg_type=MSG_PING, ttl=1,
-                  org_port=6346, payload="", msg_id="", org_ip=""):
-    if msg_type == MSG_PING:
-        print ("Creating message: PING")
-        return construct_header(ttl=ttl, org_port=org_port, length=0)
-    elif msg_type == MSG_PONG:
-        print ("Creating message: PONG")
-        header = construct_header(msg_type=msg_type, ttl=ttl,
-                                  org_port=org_port,
-                                  length=len(payload))
-        return header+payload
-    elif msg_type == MSG_BYE:
-        print ("Creating message: BYE")
-        return construct_header(msg_type=msg_type, org_port=org_port)
-    elif msg_type == MSG_JOIN:
-        print ("Creating message: JOIN")
-        header = construct_header(msg_type=msg_type, org_port=org_port,
-                                  length=len(payload))
-        return header+payload
-    elif msg_type == MSG_QUERY:
-        print ("Creating message: QUERY")
-        header = construct_header(msg_type=msg_type, ttl=ttl,
-                                  org_port=org_port, msg_id=msg_id,
-                                  length=len(payload), org_ip=org_ip)
-        return header+payload
-    elif msg_type == MSG_QHIT:
-        print ("Creating message: QHIT")
-        header = construct_header(msg_type=msg_type, ttl=ttl,
-                                  org_port=org_port, length=len(payload),
-                                  msg_id=msg_id, org_ip=org_ip)
-        return header+payload
-    else:
-        return -1
-
-
-def construct_header(ttl=1, msg_type=MSG_PING,
-                     org_port=6346, org_ip="", length=0, msg_id=""):
-    # Gnutella protocol header fields
-    version = 1
-    reserved = 0
-    # Convert IP address of sender as single number
-    if org_ip == "":
-        org_ip = ipToNum(socket.gethostbyname(socket.gethostname()))
-    else:
-        org_ip = ipToNum(org_ip)
-    if msg_id == "":
-        # Create msg_id
-        global sequence_nr
-        sequence_nr += 1
-        digest_builder = md5()
-        digest_builder.update(str(org_ip) + str(org_port) + str(time.time())
-                              + str(sequence_nr))  # test.encode('utf-8'))
-        msg_id = digest_builder.digest()
-    else:
-        msg_id = binascii.unhexlify(msg_id)
-
-    # For struct format see: https://docs.python.org/2/library/struct.html
-    return struct.pack('!BBBBHHI4s', version, ttl, msg_type, reserved,
-                       org_port, length, org_ip, msg_id)
-
-
 def handshake(socket):
-    socket.send(createMessage(msg_type=MSG_JOIN, payload=""))
+    socket.send(createMessage(msg_type=MSG_JOIN,
+                              org_ip=socket.getsockname()[0]))
     response = socket.recv(1024)
     header, payload = parseReceivedMessage(response)
     # Join was successful
@@ -205,7 +150,8 @@ def handshake(socket):
         global neighbours
         neighbours.append((header[6], header[4]))
         # Send PING type b to expand neighbourhood
-        socket.send(createMessage(ttl=5))
+        socket.send(createMessage(ttl=5, org_port=socket.getsockname()[1],
+                                  org_ip=socket.getsockname()[0]))
         response = socket.recv(1024)
         header, payload = parseReceivedMessage(response)
         if len(payload) > 0:
@@ -218,76 +164,7 @@ def handshake(socket):
             print(neighbours)
 
 
-def parseReceivedMessage(message):
-    try:
-        header_raw = struct.unpack('!BBBBHHI4s', message[:16])
-    except:
-        print("Unnable to unpack struct!")
-        return ()
-
-    # Convert single number into IP address of sender
-    ip_addr = numToIP(header_raw[6])
-    header = (header_raw[0], header_raw[1], header_raw[2], header_raw[3],
-              header_raw[4], header_raw[5], ip_addr,
-              binascii.hexlify(header_raw[7]))
-    payload = ()
-
-    # Return tuple (header, payload)
-    # PING and PONG are handled the same way
-    if header[2] == MSG_PING:
-        # No payload to look at
-        pass
-    elif header[2] == MSG_PONG:
-        if len(message) > 16:
-            # Obtain number of entries in payload
-            payload = struct.unpack('>H', message[16:18])
-            # tmp for iterating
-            tmp = 20
-            for i in range(0, payload[0]):
-                payload_raw = struct.unpack('>IHH', message[tmp:tmp+8])
-                # Convert single number into IP address
-                ip_addr = numToIP(payload_raw[0])
-                payload += ((ip_addr, payload_raw[1]),)
-                tmp += 8
-    elif header[2] == MSG_JOIN:
-        if len(message) > 16:
-            payload = binascii.hexlify(message[16:])
-    elif header[2] == MSG_QUERY:
-            # Find out how long payload is
-            length = len(message)-16
-            payload = struct.unpack('>'+str(length)+'s', message[16:])
-    elif header[2] == MSG_QHIT:
-        if len(message) > 16:
-            # Obtain number of entries in payload
-            payload = struct.unpack('>H', message[16:18])
-            # tmp for iterating
-            tmp = 20
-            for i in range(0, payload[0]):
-                payload_raw = struct.unpack('>HH4s', message[tmp:tmp+8])
-                payload += ((payload_raw[0],
-                             binascii.hexlify(payload_raw[2])),)
-                tmp += 8
-    elif header[2] == MSG_BYE:
-        # No payload to look at
-        pass
-    else:
-        print('Message does not match protocol specification.')
-        return -1
-    return (header, payload)
-
-
-# Convert IP address of sender as single number
-def ipToNum(ip_addr):
-    return struct.unpack('>L', socket.inet_aton(ip_addr))[0]
-
-
-# Convert number back to IP address
-def numToIP(num):
-    return socket.inet_ntoa(struct.pack('>L', num))
-
-
 def p2p_replying(socket):
-
     connected = True
     while connected:
         message = socket.recv(1024)
@@ -304,16 +181,14 @@ def p2p_replying(socket):
     socket.close()
 
 
+# Initialize connection before starting to reply
 def p2p_initiation(socket):
-
-    # Initialize connection before starting to reply
     handshake(socket)
     p2p_replying(socket)
 
 
+# Server sock listening on the given port.
 def make_server_socket(port):
-
-    # Server sock listening on the given port.
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((socket.gethostname(), port))
@@ -323,13 +198,23 @@ def make_server_socket(port):
     return s
 
 
-s = make_server_socket(6346)
 # Connect to existing network
-bootstrap = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-bootstrap.connect(('130.233.195.30', 6346))
-connections.append(('130.233.195.30', bootstrap))
-source = threading.Thread(target=p2p_initiation, args=[bootstrap])
-source.start()
+def bootstrap(ip, port):
+    global connections
+    bootstrap = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    bootstrap.connect((ip, port))
+    connections.append((ip, bootstrap))
+    source = threading.Thread(target=p2p_initiation, args=[bootstrap])
+    source.start()
+
+s = make_server_socket(6346)
+try:
+    bootstrap(str(sys.argv[1]), int(sys.argv[2]))
+except:
+    print("####################################################")
+    print("Node not connected to existing network. Use")
+    print("\'node.py <host-ip> <host-port>\' in order to do so.")
+    print("####################################################")
 while 1:
     (cl, address) = s.accept()
     connections.append((address[0], cl))
